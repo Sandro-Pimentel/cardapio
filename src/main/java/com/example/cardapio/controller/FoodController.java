d886601dbdcc3c5fdd347a162fb356ffc96003c1
Sandro-Pimentel <125677890+Sandro-Pimentel@users.noreply.github.com>
2024-05-13 21:27:52 -0300

    Criando o backend
    Criando o backend com os métodos get e post.

---
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -0,0 +1,32 @@
+package com.example.cardapio.controller;
+
+import com.example.cardapio.food.Food;
+import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.FoodRequestDTO;
+import com.example.cardapio.food.FoodResponseDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("com/example/cardapio/food")
+public class FoodController {
+    @Autowired
+    private FoodRepository repository;
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*") //http://localhost:3000
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data){
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*") //http://localhost:3000
+    @GetMapping
+    public List<FoodResponseDTO> getAll(){
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
+        return foodList;
+    }
+}
